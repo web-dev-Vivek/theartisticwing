c3a21264f4b63005ac4786805c880f4f9e2620ff
vivek <progamervivek2020@gmail.com>
2025-06-20 20:50:54 +0530

    done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -35,6 +35,7 @@ function Cart() {
   }
 
   return (
+
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="mb-8">
@@ -241,6 +242,7 @@ function Cart() {
         </div>
       </div>
     </div>
+
   );
 }
 
